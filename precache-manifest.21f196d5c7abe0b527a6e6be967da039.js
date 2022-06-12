11258aaeba4a2932cf1776f7913d9fff595517d3
lavensoft <nhatsdevil@gmail.com>
2022-06-13 01:12:49 +0700

    UPDATE

---
+++ precache-manifest.21f196d5c7abe0b527a6e6be967da039.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8344d929e71e4fb081ab2d12503860",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5767194741e0c44b2cec",
+    "url": "/static/css/main.3bef68d2.chunk.css"
+  },
+  {
+    "revision": "43ace4e5a614de4a700a",
+    "url": "/static/js/2.d63d5c32.chunk.js"
+  },
+  {
+    "revision": "a0d60f63c78060381a8612ff79097ea8",
+    "url": "/static/js/2.d63d5c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5767194741e0c44b2cec",
+    "url": "/static/js/main.43e311c3.chunk.js"
+  },
+  {
+    "revision": "af947e65f148cf8ee2c4",
+    "url": "/static/js/runtime-main.022e8f21.js"
+  },
+  {
+    "revision": "650d40b017d7e6e5e068314d59661bcf",
+    "url": "/static/media/arrow.650d40b0.svg"
+  },
+  {
+    "revision": "7d40d0b47cf38c597f0d8424240334cf",
+    "url": "/static/media/checkmark.7d40d0b4.svg"
+  },
+  {
+    "revision": "e7ca074e97413a6a8740d8fc4fec0734",
+    "url": "/static/media/disabledArrow.e7ca074e.svg"
+  }
+]);
